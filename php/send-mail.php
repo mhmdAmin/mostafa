c8c084c6426aadebe59be60da5a12b65b354f080
mhmd-amin <mhmd_amin@hotmail.com>
2025-08-04 00:56:51 +0300

    WIP

--- php/send-mail.php
+++ php/send-mail.php
@@ -1,11 +1,15 @@
 <?php
 
+ini_set( 'display_errors', 1 );
+ini_set( 'display_startup_errors', 1 );
+error_reporting( E_ALL );
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\OAuth;
 
 
-require __DIR__ . '/vendor/autoload.php';
 
 
 
@@ -57,6 +61,7 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 			exit;
 	}
 
+	require __DIR__ . '/vendor/autoload.php';
 	$dotenv = Dotenv\Dotenv::createImmutable( __DIR__ );
 	$dotenv->load();
 
