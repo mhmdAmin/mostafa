2ce9396dbf78caef58486d04dee9c41700b47961
mhmd-amin <mhmd_amin@hotmail.com>
2025-08-05 02:51:46 +0300

    WIP

--- php/send-mail.php
+++ php/send-mail.php
@@ -10,41 +10,46 @@ require 'vendor/autoload.php'; // Adjust if needed
 
 // Handle form POST
 if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
-	$name    = trim( $_POST['UserName'] ?? '' );
-	$email   = trim( $_POST['UserEmail'] ?? '' );
-	$subject = trim( $_POST['subject'] ?? '' );
-	$message = trim( $_POST['message'] ?? '' );
-}
-if ( ! filter_var( $email, FILTER_VALIDATE_EMAIL ) ) {
-	echo json_encode(
-		array(
-			'status'  => 'error',
-			'message' => 'Invalid email address.',
-		)
-	);
-	exit;
+	$name         = trim( $_POST['UserName'] ?? '' );
+	$sender_email = $_POST['UserEmail'] ?? '';
+	$subject      = trim( $_POST['subject'] ?? 'NO MESSAGE FROM THE SENDER' );
+	$message      = trim( $_POST['message'] ?? '' );
+
 }
 
 
+// if ( ! filter_var( $sender_email, FILTER_VALIDATE_EMAIL ) ) {
 
-$mail = new PHPMailer( true );
+// echo json_encode(
+// array(
+// 'status'  => 'error',
+// 'message' => 'Invalid email address.',
+// )
+// );
+// exit;
+// }
+
+
+$hoster_email = 'bastpet.uk@gmail.com';
+$mail         = new PHPMailer( true );
 
 try {
 	// Server settings
 	$mail->isSMTP();
 	$mail->Host       = 'smtp.gmail.com';
 	$mail->SMTPAuth   = true;
-	$mail->Username   = 'bastpet.uk@gmail.com';        // Your Gmail
+	$mail->Username   = $hoster_email;        // Your Gmail
 	$mail->Password   = 'bqqzzthczuaijqht';          // 16-char App Password for Gmail
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 	$mail->Port       = 587;
 
 	// Recipients
-	$mail->setFrom( $email, 'bastpet.co.uk[ Contact Form ]' );
-	$mail->addAddress( 'bastpet.uk@gmail.com' );         // Send to yourself
-	$mail->addAddress( 'mostafa.amin@bastpet.co.uk' );         // Send to another email address
+	$mail->setFrom( $hoster_email, '[ contact Form ]' );
+	$mail->addAddress( $hoster_email );         // Send to yourself
+	$mail->addAddress( $sender_email );         // Add sender email address
+	$mail->addAddress( 'mostafa.amin@bastpet.co.uk' );   // Optional: Forward copy to another email (can also be done via Gmail settings)
 
-	// Optional: Forward copy to business email (can also be done via Gmail settings)
+	$mail->addReplyTo( $sender_email );         // Reply to sender email address
 
 	// Content
 	$mail->isHTML( true );
@@ -52,9 +57,10 @@ try {
 	$mail->Encoding = 'base64';
 	$mail->Subject  = $subject;
 	$mail->Body     = $message;
-	$mail->AltBody  = $message;
+	$mail->AltBody  = 'No Messages body';
 
 	$mail->send();
+
 	// echo json_encode(['success' => true, 'message' => 'Message has been sent']);
 } catch ( Exception $e ) {
 	echo json_encode(
