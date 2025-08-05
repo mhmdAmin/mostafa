31eab11a74ec4a6a0cfa885fc4e5393ee8a50834
mhmd-amin <mhmd_amin@hotmail.com>
2025-08-05 03:08:43 +0300

    WIP

--- php/send-mail.php
+++ php/send-mail.php
@@ -57,7 +57,7 @@ try {
 	$mail->CharSet  = 'UTF-8';
 	$mail->Encoding = 'base64';
 	$mail->Subject  = $subject;
-	$mail->Body     = "Name: $name\nEmail: $sender_email\nMessage: $message"
+	$mail->Body     = "Name: $name\n" . "Email: $sender_email\n" . "Subject: $subject\n" . "Message: $message";
 
 
 	$mail->AltBody = 'No Messages body';
